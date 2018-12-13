c45fd8373a70bcd34a353c1a950f9ad263f2e438
Cameron Husselbury <39767474+vindicat0r@users.noreply.github.com>
2018-12-13 20:03:25 +0000

    Update fibonacci.py
    A program which prints the fibonacci sequence to the Nth term.

--- fibonacci.py
+++ fibonacci.py
@@ -13,8 +13,8 @@ sequence_list = []
 term = 0
 
 for x in range(n):
-  term = (((1 + sqrt(5))/2)**x - ((1 - sqrt(5))/2)**x)/sqrt(5)
-  sequence_list.append(int(term))
+    term = (((1 + sqrt(5))/2)**x - ((1 - sqrt(5))/2)**x)/sqrt(5)
+    sequence_list.append(int(term))
 
 sequence_list_string = [str(x) for x in sequence_list]
 print('The fibonacci sequence to the ' + str(n) + 'th place (including 0) is:')
