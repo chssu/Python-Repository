973914af9c3f39b604bd6c3f975092e503eb1a8a
Cameron Husselbury <39767474+vindicat0r@users.noreply.github.com>
2018-12-13 20:06:59 +0000

    Prints the Fibonacci sequence to the Nth term

--- fibonacci.py
+++ fibonacci.py
@@ -13,7 +13,7 @@ sequence_list = []
 term = 0
 
 for x in range(n):
-   term = (((1 + sqrt(5))/2)**x - ((1 - sqrt(5))/2)**x)/sqrt(5)
+    term = (((1 + sqrt(5))/2)**x - ((1 - sqrt(5))/2)**x)/sqrt(5)
     sequence_list.append(int(term))
 
 sequence_list_string = [str(x) for x in sequence_list]
